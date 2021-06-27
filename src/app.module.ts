190b7ec9dceb2c218772b4feedf9377969b4fd8e
Yobi <ordinary.yobi@gmail.com>
2021-06-27 17:24:35 +0900

    필요할 것 같은 module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ServiceModule } from './modules/service/service.module';
+import { CategoryModule } from './modules/category/category.module';
+import { ReviewModule } from './modules/review/review.module';
+import { ProductModule } from './modules/product/product.module';
+import { OrderModule } from './modules/order/order.module';
+import { CartModule } from './modules/cart/cart.module';
+import { QnaModule } from './modules/qna/qna.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+import { GraphqlModule } from './graphql/graphql.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ServiceModule,
+    CategoryModule,
+    ReviewModule,
+    ProductModule,
+    OrderModule,
+    CartModule,
+    QnaModule,
+    PaymentModule,
+    DatabaseModule,
+    ConfigModule,
+    GraphqlModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
